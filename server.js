b93b45ecf920caec8dca58375236fdf0b12bfcf8
Saurabh Anand <anandsaurabh46@gmail.com>
2021-11-26 14:26:17 +0530

    CN project

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const path = require("path")
+const port = process.env.PORT || 5000
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public/build")));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve("public/build/index.html"));
+  });
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(port, () => console.log("server is running on port "+port))
